e13e1851b1f105d6a1c0a45ac0f6a0306c0b1f6f
Renard Bergson <renardrock@gmail.com>
2023-06-26 15:28:14 -0300

    obtendo a lista de produtos cadastrados

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+const productsList = document.querySelector('#products-list')
+
+// GETTING THE PRODUCTS LIST
 fetch('http://localhost:8080/api/products')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        const productsHtml = data.map(product => 
+            `<li>${product.name} - ${product.brand} - ${product.price}</li>`
+        ).join('')
+
+        console.log(productsHtml)
+
+        productsList.innerHTML = productsHtml
+
+        /* data.forEach(product => {
+            const productsHtml = `<li>${product.name} - ${product.brand} - ${product.price}</li>`
+            
+            productsList.innerHTML += productsHtml
+        }) */
+    })
